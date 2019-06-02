0c404368e08d3c52476b32cd0c3e531febb23e05
andreyprogrammer <andreyprogrammer@mail.ru>
2019-06-02 16:09:44 +0300

    задание к уроку 3 выполнено

--- src/Lesson_3/PhoneBook.java
+++ src/Lesson_3/PhoneBook.java
@@ -1,4 +1,21 @@
 package Lesson_3;
 
-public class PhoneBook {
+import java.util.*;
+
+public class PhoneBook extends HashMap {
+    public void add(String name, String phone) {
+        this.put(phone, name);
+    }
+
+    public List<String> get(String fam) {
+        Iterator<Map.Entry<String, String>> iter = this.entrySet().iterator();
+        List<String> phoneNumbers = new ArrayList<>();
+        while (iter.hasNext()) {
+            Map.Entry<String, String> num = iter.next();
+            if (num.getValue() == fam) {
+                phoneNumbers.add(num.getKey());
+            }
+        }
+        return phoneNumbers;
+    }
 }
